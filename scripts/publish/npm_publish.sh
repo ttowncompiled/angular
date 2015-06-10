79b5f67b4ef1c97c3e61d091546e12eaeaa32e9f
Ian Riley <ian.riley.915@gmail.com>
2015-06-10 14:15:46 -0700

    chore(npm): Removes AtScript and es6 from npm publication.
    Removes AtScript and es6 directories as well as adds TypeScript to our npm publication.

--- scripts/publish/npm_publish.sh
+++ scripts/publish/npm_publish.sh
@@ -12,29 +12,16 @@ NPM_DIR=$ROOT_DIR/dist/npm
 rm -fr $NPM_DIR
 FILES='!(test|e2e_test|docs)'
 
-function publishRttsAssert {
-  NAME='rtts_assert'
-  PUBLISH_DIR=$NPM_DIR/$NAME
-  rm -fr $PUBLISH_DIR
-  mkdir -p $PUBLISH_DIR
-
-  mkdir -p $PUBLISH_DIR/es6
-  cp -r $ROOT_DIR/dist/js/prod/es6/$NAME/$FILES $PUBLISH_DIR/es6
-
-  cp -r $ROOT_DIR/dist/js/cjs/$NAME/$FILES $PUBLISH_DIR
-  npm publish $PUBLISH_DIR
-}
-
 function publishModule {
   NAME=$1
   PUBLISH_DIR=$NPM_DIR/$NAME
   rm -fr $PUBLISH_DIR
   mkdir -p $PUBLISH_DIR
 
-  mkdir -p $PUBLISH_DIR/es6/dev
-  cp -r $ROOT_DIR/dist/js/dev/es6/$NAME/$FILES $PUBLISH_DIR/es6/dev
-  mkdir -p $PUBLISH_DIR/es6/prod
-  cp -r $ROOT_DIR/dist/js/prod/es6/$NAME/$FILES $PUBLISH_DIR/es6/prod
+  mkdir -p $PUBLISH_DIR/dev
+  cp -r $ROOT_DIR/dist/js/dev/$NAME/$FILES $PUBLISH_DIR/dev
+  mkdir -p $PUBLISH_DIR/prod
+  cp -r $ROOT_DIR/dist/js/prod/$NAME/$FILES $PUBLISH_DIR/prod
   mkdir -p $PUBLISH_DIR/ts
   cp -r $ROOT_DIR/modules/$NAME/$FILES $PUBLISH_DIR/ts
 
@@ -43,6 +30,5 @@ function publishModule {
   npm publish $PUBLISH_DIR
 }
 
-publishRttsAssert
 publishModule angular2
 publishModule benchpress
